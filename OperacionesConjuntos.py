5442d819a0a782b15dc8b516c4b4928e3e2daa1d
anrumo <anrumo232@gmail.com>
2024-09-06 17:26:31 -0500

    Minimos cambios

--- OperacionesConjuntos.py
+++ OperacionesConjuntos.py
@@ -4,8 +4,9 @@ from matplotlib_venn import venn2, venn3
 
 #Funcion para calcular la Union de conjuntos
 def union_conjuntos(*conjuntos):
+    conjuntoss = conjuntos[0]
     resultado = []
-    for conjunto in conjuntos:
+    for conjunto in conjuntoss:
         for elemento in conjunto:
             if elemento not in resultado:
                 resultado.append(elemento)
@@ -13,20 +14,26 @@ def union_conjuntos(*conjuntos):
 
 #Funcion para calcular la interseccion entre multiples conjuntos
 def interseccion(*conjuntos):
+    conjuntoss = conjuntos[0]
     resultado = []
     
-    if len(conjuntos) == 0:
+    if len(conjuntoss) == 0:
         return resultado
-    elif len(conjuntos) == 2:
-        resultado = interseccion2(conjuntos[0], conjuntos[1])
+    elif len(conjuntoss) == 2:
+        resultado = interseccion2(conjuntoss[0], conjuntoss[1])
         print("La interseccion es " + str(resultado))
-        graficar_conjuntos_2(conjuntos[0], conjuntos[1], "A", "B")
-    elif len(conjuntos) == 3:
-        resultado = interseccion3(conjuntos[0], conjuntos[1], conjuntos[2])
+        graficar_conjuntos_2(conjuntoss[0], conjuntoss[1], "A", "B")
+    elif len(conjuntoss) == 3:
+        resultado = interseccion3(conjuntoss[0], conjuntoss[1], conjuntoss[2])
         print(resultado)
-        graficar_conjuntos_3(conjuntos[0], conjuntos[1], conjuntos[2], "A", "B", "C")
+        graficar_conjuntos_3(conjuntoss[0], conjuntoss[1], conjuntoss[2], "A", "B", "C")
     else:
-        print("hola")
+        resultado = conjuntoss[0].copy()
+        for conjunto in conjuntoss[1:]:
+            for elemento in conjuntoss[0]:
+                if not elemento in conjunto and elemento in resultado:
+                    resultado.remove(elemento)
+        print("La intersección general es " + str(resultado))
 
 
 def interseccion2(a, b):
@@ -52,28 +59,33 @@ def interseccion3(a, b, c):
         if elemento in b_c:
             b_c.remove(elemento)
 
-    resultado = "La intersección general es: " + str(a_b_c) + "\n" + """La intersección entre 
-    solo A y B es: """ + str(a_b) + "\n" + """La intersección entre solo A y C es: """ + str(a_c) + """\n
-    La intersección entre solo B y C es: """ + str(b_c) 
+    x = "La intersección general es: " + str(a_b_c) + "\n" + "La intersección entre solo A y B es: "
+    y = str(a_b) + "\n" + "La intersección entre solo A y C es: " + str(a_c) + "\n"
+    z = "La intersección entre solo B y C es: " + str(b_c) 
 
-    return resultado
+    return x + y + z
 
 #Funcion para calcular la diferencia entre varios conjuntos
 def diferencia_conjuntos(conjunto_base, *conjuntos):
-    resultado = [elemento for elemento in conjunto_base if all(elemento not in conjunto for conjunto in conjuntos)]
+    conjuntoss = conjuntos[0]
+    resultado = [elemento for elemento in conjunto_base if all(elemento not in conjunto for conjunto in conjuntoss)]
     return resultado
 
 #Funcion para calcular la diferencia simetrica entre
-def diferencia_simetrica_conjuntos(*conjuntos):
+def diferencia_simetrica_conjuntos(conjuntoA, conjuntoB):
+
+    inter = interseccion2(conjuntoA, conjuntoB)
+
     resultado = []
-    historial = []
-    for conjunto in conjuntos:
-        for elemento in conjunto:
-            if elemento not in resultado and elemento not in historial:
-                resultado.append(elemento)
-            elif elemento not in historial:
-                resultado.remove(elemento)
-                historial.append(elemento)
+
+    for elemento in conjuntoA:
+        if elemento not in inter:
+            resultado.append(elemento)
+
+    for elemento in conjuntoB:
+        if elemento not in inter:
+            resultado.append(elemento)
+        
     return resultado
 
 
@@ -142,12 +154,93 @@ def graficar_conjuntos_3(a, b, c, nombre_a, nombre_b, nombre_c):
     plt.title("Diagrama de venn")
     plt.show()
 
+def ingresar_conjunto(cantidad):
+    conjuntos = []
+    print("Ingresa los elementos del conjunto (escribe 'fin' para terminar):")
+    i = 0
+    for i in range(cantidad):
+        conjunto = []
+        while True:
+            elemento = input("Añade el elemento al conjunto #" + str(i+1) + ": ")
+
+            if elemento.lower() == 'fin':  # Termina el ciclo si el usuario escribe 'fin'
+                break
+            else:
+                conjunto.append(int(elemento))  # Agrega el elemento al conjunto
+        conjuntos.append(conjunto)
+    return conjuntos
+
+def mostrar_menu(cant):
+
+    print("\n--- Menú Principal ---")
+    print("1. Realizar la unión de los conjuntos.")
+    print("2. Realizar la intersección.")
+    print("3. Realizar la diferencia entre los conjuntos.")
+    if cant == 2:
+        print("4. Realizar la diferencia simetrica.")
+        print("5. Verificar subconjunto.")
+    print("6. Salir")    
+
+
+def opcion_1(conjuntos):
+
+    resultado = union_conjuntos(conjuntos)
+    print("La unión de los conjuntos es " + str(resultado))
+
+def opcion_2(conjuntos):
+
+    resultado = interseccion(conjuntos)
+    print(resultado)
+
+def opcion_3(conjuntos):
+
+    indice = int(input("Dame el numero del conjunto al cual se le restaran el resto: "))
+    elemento = conjuntos.pop(indice-1)
+    resultado = diferencia_conjuntos(elemento, conjuntos) 
+    print("El resultado de la resta es " + str(resultado))
+
+def opcion_4(conjuntos):
+     
+    resultado = diferencia_simetrica_conjuntos(conjuntos[0], conjuntos[1])
+    print("El resultado de la diferencia simetrica es " + str(resultado))
+
+def opcion_5(conjuntos):
+
+    if es_subconjunto(conjuntos[0], conjuntos[1]):
+        print("El conjunto #2 es subconjunto del conjunto #2")
+    else:
+        print("El conjunto #2 no es subconjunto del conjunto #1")
+
+
+def salir():
+    print("\nSaliendo del programa...")
+    exit()
+
+def ejecutar_menu():
+    cantidad = int(input("Cuantos conjuntos quiere ingresar: "))
+
+    conjuntos = ingresar_conjunto(cantidad)
+
+    while True:
+        mostrar_menu(len(conjuntos))
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            opcion_1(conjuntos)
+        elif opcion == "2":
+            opcion_2(conjuntos)
+        elif opcion == "3":
+            opcion_3(conjuntos)
+        elif opcion == "4":
+            opcion_4(conjuntos)
+        elif opcion == "5":
+            opcion_5(conjuntos)
+        elif opcion == "6":
+            salir()
+        else:
+            print("\nOpción no válida. Intenta de nuevo.")
+
 def main():
-    a = [1,2,3,4]
-    b = [3,4,5,6,7]
-    c = [2,3,8,9]
-    #graficar_conjuntos_2(a,b, "A", "B")
-    #graficar_conjuntos_3(a,b,c, "A", "B", "C")
-    interseccion(a, b, c)
+    ejecutar_menu()
 
 main()
